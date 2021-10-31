e00f8a6805ba084ab1a39a9e5082ff220dbc0592
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2021-10-31 20:51:21 -0300

    adiciona css e corpo do html

---
+++ src/components/Register.tsx
@@ -0,0 +1,29 @@
+export default function Register() {
+  return (
+    <div className='form'>
+      <div className='form-container'>
+        <h1>Cadastro</h1>
+
+        <p>Nome</p>
+        <input type='text' />
+
+        <p>Senha</p>
+        <input type='password' name='' id='' />
+
+        <p>E-mail</p>
+        <input type='text' name='' id='' />
+
+        <p>CPF</p>
+        <input type='text' />
+
+        <p>Telefone</p>
+        <input type='text' />
+
+
+        <button>Cadastrar</button>
+
+
+      </div>
+    </div>
+  )
+}
