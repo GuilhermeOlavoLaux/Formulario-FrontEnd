c941cc829137a01f3595335a22dc3820520dcbe8
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2021-11-03 17:40:25 -0300

    adiciona manipulação de inputs

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,28 +1,61 @@
+import { ChangeEvent, useState } from 'react'
+// const emailRegex = require('../utils/validators')
+
+interface IFields {
+  name: string
+  password: string
+  eMail: string
+  cpf: string
+  telephone: string
+}
+
 export default function Register() {
+  const [fields, setFields] = useState<IFields>({} as IFields)
+
   return (
     <div className='form'>
       <div className='form-container'>
         <h1>Cadastro</h1>
-
         <p>Nome</p>
-        <input type='text' />
+        <input
+          type='text'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, name: event.target.value })
+          }
+        />
 
         <p>Senha</p>
-        <input type='password' name='' id='' />
+        <input
+          type='password'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, password: event.target.value })
+          }
+        />
 
         <p>E-mail</p>
-        <input type='text' name='' id='' />
-
+        <input
+          type='text'
+          className='email-field'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, eMail: event.target.value })
+          }
+        />
         <p>CPF</p>
-        <input type='text' />
-
+        <input
+          type='text'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, cpf: event.target.value })
+          }
+        />
         <p>Telefone</p>
-        <input type='text' />
-
+        <input
+          type='text'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, telephone: event.target.value })
+          }
+        />
 
         <button>Cadastrar</button>
-
-
       </div>
     </div>
   )
