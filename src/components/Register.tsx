acf9cf8eccd434f146b2b780ea351c4f31c4a278
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2021-11-09 23:28:05 -0300

    adiciona mensagens de validação

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,11 +2,9 @@ import { ChangeEvent, useState } from 'react'
 import api from '../routes/routes'
 import validateEmail from '../utils/eMailValidator'
 import validateTelephone from '../utils/telephoneNumberValidator'
-import cpfValidator from '../utils/cpfValidator'
-
+import validateCpf from '../utils/cpfValidator'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import validateCpf from '../utils/cpfValidator'
 
 interface IFields {
   name: string
@@ -18,14 +16,18 @@ interface IFields {
 
 export default function Register() {
   const [fields, setFields] = useState<IFields>({} as IFields)
+
+  //toast ids
   const idToastFields = 'idToastFields'
   const idToastValidEmail = 'idToastValidEmail'
   const idSuccesPost = 'idSuccesPost'
   const idToastValidCpf = 'idToastValidCpf'
 
+  const emailAdvice = window.document.getElementById('emailAdvice')
+  const telephoneAdvice = window.document.getElementById('telephoneAdvice')
+  const cpfAdvice = window.document.getElementById('cpfAdvice')
+
   async function saveUser() {
-
-
     if (!fields.name || !fields.password || !fields.eMail || !fields.cpf) {
       toast.error('Preencha todos os campos obrigatórios', {
         toastId: idToastFields,
@@ -48,6 +50,24 @@ export default function Register() {
         draggable: false,
         progress: undefined
       })
+
+      if (emailAdvice !== null) {
+        emailAdvice.style.display = 'block'
+      }
+    } else if (!validateCpf(fields.cpf)) {
+      toast.error('Insira um cpf válido', {
+        toastId: idToastValidCpf,
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined
+      })
+      if (cpfAdvice !== null) {
+        cpfAdvice.style.display = 'block'
+      }
     } else if (fields.telephone && !validateTelephone(fields.telephone)) {
       toast.error('Insira um telefone válido', {
         toastId: idToastValidEmail,
@@ -59,17 +79,9 @@ export default function Register() {
         draggable: false,
         progress: undefined
       })
-    }  else if (!validateCpf(fields.cpf)) {
-      toast.error('Insira um cpf válido', {
-        toastId: idToastValidCpf,
-        position: 'top-right',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: false,
-        progress: undefined
-      })
+      if (telephoneAdvice !== null) {
+        telephoneAdvice.style.display = 'block'
+      }
     } else {
       await api.post('/users', fields)
       toast.success('Usuário criado com sucesso', {
@@ -82,6 +94,24 @@ export default function Register() {
         draggable: false,
         progress: undefined
       })
+      //@ts-ignore
+      telephoneAdvice.style.display = 'none'
+      //@ts-ignore
+      cpfAdvice.style.display = 'none'
+      //@ts-ignore
+      emailAdvice.style.display = 'none'
+      //@ts-ignore
+      document.getElementById('nameInput').value = ''
+
+      //@ts-ignore
+      document.getElementById('passwordInput').value = ''
+      //@ts-ignore
+      document.getElementById('emailInput').value = ''
+      //@ts-ignore
+      document.getElementById('cpfInput').value = ''
+      //@ts-ignore
+      document.getElementById('telephoneInput').value = ''
+      setFields({ name: '', password: '', eMail: '', cpf: '', telephone: '' })
     }
   }
 
@@ -105,6 +135,7 @@ export default function Register() {
         <p>Nome</p>
         <input
           type='text'
+          id='nameInput'
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, name: event.target.value })
           }
@@ -113,6 +144,7 @@ export default function Register() {
         <p>Senha</p>
         <input
           type='password'
+          id='passwordInput'
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, password: event.target.value })
           }
@@ -121,26 +153,40 @@ export default function Register() {
         <p>E-mail</p>
         <input
           type='text'
+          id='emailInput'
           className='email-field'
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, eMail: event.target.value })
           }
         />
+        <p className='advice' id='emailAdvice'>
+          *Utilize o seguinte formato: email@email.com
+        </p>
+
         <p>CPF</p>
         <input
           type='text'
+          id='cpfInput'
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, cpf: event.target.value })
           }
-          
         />
+        <p className='advice' id='cpfAdvice'>
+          *Utilize o seguinte formato: 000.000.000-00
+        </p>
+
         <p>Telefone</p>
         <input
           type='text'
+          id='telephoneInput'
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, telephone: event.target.value })
           }
         />
+
+        <p className='advice advice-telephone' id='telephoneAdvice'>
+          *Utilize o seguinte formato: (51)99999-9999
+        </p>
 
         <button onClick={saveUser}>Cadastrar</button>
       </div>
--- src/utils/telephoneNumberValidator.ts
+++ src/utils/telephoneNumberValidator.ts
@@ -1,6 +1,5 @@
 export default function validateTelephone(telephone: string) {
-    var regex = new RegExp('^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$'); 
+  const regex = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/
 
-    return regex.test(telephone)
-  }
-  
+  return regex.test(telephone)
+}
